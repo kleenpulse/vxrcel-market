b3ff09bd7e951ab6d124b0406533660cb39bc28c
Vxrcel <skyeparagon@gmail.com>
2024-02-10 18:16:51 +0100

    Loading

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import LoadingSpinner from "@/components/LoadingSpinner";
+import React from "react";
+
+const LoadingAuth = () => {
+	return <LoadingSpinner />;
+};
+
+export default LoadingAuth;
